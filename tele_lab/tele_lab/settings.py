97634fb291f4230b1a4351f028499f7b7660a220
Diego Sosa <diego.sosa0704@gmail.com>
2019-06-06 20:52:13 -0500

    Configuración de correo electrónico.

--- tele_lab/tele_lab/settings.py
+++ tele_lab/tele_lab/settings.py
@@ -157,8 +157,8 @@ EMAIL_USE_TLS = True
 
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'diego.sosa@uptc.edu.co'  # -> Email address
-EMAIL_HOST_PASSWORD = 'alduin123'  # -> Email password
+EMAIL_HOST_USER = ''  # -> Email address
+EMAIL_HOST_PASSWORD = ''  # -> Email password
 EMAIL_PORT = 587
 # DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
