9b97b2a4817c3b891edbfbb5f2a48144fb57675b
Diego Sosa <diego.sosa0704@gmail.com>
2019-08-20 23:49:17 -0500

    Versión inicial de lista de prestamos.

--- tele_lab/front_end/src/containers/Admin/Return.jsx
+++ tele_lab/front_end/src/containers/Admin/Return.jsx
@@ -1,40 +1,70 @@
 import React, {Component} from 'react'
 import {Grid, Header, Input, Rail, Segment, Sticky, Visibility} from "semantic-ui-react";
 import LoansReturn from "../../components/Admin/Return/LoansReturn";
-import _ from "lodash";
 import {connect} from "react-redux";
 import {returnComponent} from "../../actions";
 import ModalEndLoan from "../../components/Admin/Return/ModalEndLoan";
 import * as PropTypes from 'prop-types'
 import LoanInformation from "../../components/Admin/Return/LoanInformation";
 
+const queryParams = ['_limit', 'q', '_page'];
+const regex = new RegExp("^[a-zA-Z0-9 ]+$");
+
 class ReturnComponents extends Component {
   state = {
     isLoading: false,
     searchQuery: '',
     context: null,
-    calculations: {
-      topPassed: false,
-      bottomPassed: false,
-      topVisible: false,
-      bottomVisible: false,
-    },
+    q: '',
+    _page: 0,
+    _limit: 20,
   };
 
-  handleUpdate = (e, { calculations }) => this.setState({ calculations });
+  onBottomVisible = (event) => {
+    this.setState({_page: this.state._page + 1});
+    this.loadData({_page: this.state._page})
+  };
+
+  handleOfChangeInput = (event, {name, value}) => {
+    if (regex.test(value)) {
+      this.onSubmitFilter(value);
+    }
+  };
+
+  onSubmitFilter = (filter) => {
+    if (filter !== this.state.q) {
+      this.setState({q: filter, _page: 1});
+      this.loadData({q: filter, _page: 1});
+    }
+  };
+
+  loadData(params) {
+    const current = this.state;
+    queryParams.forEach(function (element) {
+      if (!(element in params)) {
+        params[element] = current[element];
+      }
+    });
+    const esc = encodeURIComponent;
+    const query = Object.keys(params)
+      .map(k => esc(k) + '=' + esc(params[k]))
+      .join('&');
+    this.props.getLoans(query);
+  }
 
   handleContextRef = ref => this.setState({context: ref});
 
   constructor(props) {
     super(props);
-    this.getLoanSearch = this.getLoanSearch.bind(this)
+    // this.getLoanSearch = this.getLoanSearch.bind(this)
   }
 
   componentDidMount() {
-    this.props.getPendingLoans();
+    this.loadData({});
+    // this.props.getPendingLoans();
   }
 
-  getLoanSearch(data) {
+  /*getLoanSearch(data) {
     this.setState({isLoading: true});
     setTimeout(() => {
       const re = new RegExp(_.escapeRegExp(data.value), 'i');
@@ -42,10 +72,10 @@ class ReturnComponents extends Component {
       let results = _.filter(this.props.pendingLoans, isMatch);
       this.setState({result: results, isLoading: false});
     }, 300)
-  }
+  }*/
 
   render() {
-    const { calculations } = this.state;
+    const { _page } = this.state;
     return (
       <div ref={this.handleContextRef}>
         <Grid centered columns='equal'>
@@ -58,22 +88,26 @@ class ReturnComponents extends Component {
               <Grid padded relaxed stackable className='grid-return'>
                 <Grid.Row columns={'equal'} className='row-return-components'>
                   <Grid.Column className='column-return-loans'>
-                    <Visibility offset={[10, 10]} onUpdate={this.handleUpdate}>
+                    <Visibility
+                      once={false}
+                      continuous={false}
+                      onBottomVisible={this.onBottomVisible}
+                    >
                       <Input loading={this.state.isLoading}
                              icon='users'
                              iconPosition='left'
                              placeholder='Buscar...'
-                             onChange={_.debounce((event, data) => this.getLoanSearch(data), 500, {leading: true})}/>
-                      <LoansReturn loansReturn={this.state.result !== undefined ?
-                        this.state.result :
-                        this.props.pendingLoans}
+                        /*onChange={_.debounce((event, data) => this.getLoanSearch(data), 500, {leading: true})}*/
+                             onChange={this.handleOfChangeInput}
+                      />
+                      <LoansReturn loansReturn={this.props.loans}
                       />
                     </Visibility>
                   </Grid.Column>
                   <Grid.Column className='column-return-components'>
                     <Rail className='my_rail' position='right'>
                       <Sticky context={this.state.context} pushing offset={20}>
-                        {calculations.bottomVisible.toString()}
+                        {_page.toString()}
                         <LoanInformation/>
                       </Sticky>
                     </Rail>
@@ -90,6 +124,7 @@ class ReturnComponents extends Component {
 
 const mapStateToProps = state => {
   return {
+    loans: state.returnComponent.loans,
     pendingLoans: state.returnComponent.pendingLoans,
     endLoan: state.returnComponent.endLoan,
   };
@@ -99,6 +134,9 @@ const mapDispatchToProps = dispatch => {
   return {
     getPendingLoans: () => {
       return dispatch(returnComponent.getPendingLoans());
+    },
+    getLoans: (query) => {
+      return dispatch(returnComponent.getLoans(query));
     },
   };
 };
